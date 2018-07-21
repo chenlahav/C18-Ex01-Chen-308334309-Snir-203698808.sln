029c332231521339becbc29850d2d204f4a7e3f7
chen lahav <chenlahav6@gmail.com>
2018-07-21 17:48:52 +0300

    login

--- FacebookApp/Form1.Designer.cs
+++ FacebookApp/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(12, 26);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(199, 62);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 529);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
     }
 }
 
--- FacebookApp/Form1.cs
+++ FacebookApp/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Facebook;
+using FacebookWrapper;
 
 namespace FacebookApp
 {
     public partial class Form1 : Form
     {
+        private FacebookWrapper.ObjectModel.User m_LoggedInUser;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            LoginResult result = FacebookService.Login("1430451463721328", "email");
+            m_LoggedInUser = result.LoggedInUser;
         }
     }
 }
